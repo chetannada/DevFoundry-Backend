8f6c76cd2ed681967dfa43b8f917694d42c9721d
Chetan Nada <call2chetannada@gmail.com>
2025-08-17 22:05:50 +0530

    Timestamps added in projectmodel

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -3,7 +3,7 @@ const Project = require("../models/projectsModel");
 // GET all projects
 exports.getAllCraftedProjects = async (req, res) => {
   try {
-    const projects = await Project.find();
+    const projects = await Project.find().sort({ updatedAt: -1 });
     res.json(projects);
   } catch (err) {
     console.error("Error fetching projects:", err);
--- models/projectsModel.js
+++ models/projectsModel.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
-const projectSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  codeUrl: { type: String, required: true },
-  liveUrl: { type: String, required: true },
-  contributor: { type: String, required: true },
-  contributorAvatar: { type: String, required: true },
-  contributorUrl: { type: String, required: true },
-  role: {
-    type: String,
-    enum: ["admin", "contributor"],
-    default: "contributor",
+const projectSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    codeUrl: { type: String, required: true },
+    liveUrl: { type: String, required: true },
+    contributor: { type: String, required: true },
+    contributorAvatar: { type: String, required: true },
+    contributorUrl: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["admin", "contributor"],
+      default: "contributor",
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Project = mongoose.model("CraftedProjects", projectSchema);
 
