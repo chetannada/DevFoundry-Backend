046729dd33b924f2a9893dd9dfd677af4a3d036c
Chetan Nada <call2chetannada@gmail.com>
2026-05-16 22:52:09 +0530

    feat: implement country and city apis with environment configuration

--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { getWeather } = require("../controllers/weatherController");
+const { getWeather, getCountries, getCities } = require("../controllers/weatherController");
 
 router.get("/", getWeather);
+router.get("/countries", getCountries);
+router.get("/cities", getCities);
 
 module.exports = router;
